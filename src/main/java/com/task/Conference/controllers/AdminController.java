6d4c1ec35912ab969410950233b119f9ebf35a9b
sorokaa <sasha20999@outlook.com>
2021-06-20 16:47:31 +0300

    Optimized endpoints of Admin controller

--- src/main/java/com/task/Conference/controllers/AdminController.java
+++ src/main/java/com/task/Conference/controllers/AdminController.java
@@ -4,37 +4,41 @@ import com.task.Conference.entities.User;
 import com.task.Conference.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 public class AdminController {
 
-    @Autowired
     private UserService userService;
 
-    @GetMapping("/adminPage")
-    public String usersList(Map<String, Object> model) {
-
-        Iterable<User> users = userService.findAll();
-        model.put("users", users);
-
-        return "admin/adminPage";
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
     }
 
-    @PostMapping("/adminPage")
+    @GetMapping("/admin-page")
+    public String usersList(Map<String, Object> model) {
+
+        List<User> users = userService.findAll();
+        model.put("users", users);
+        return "admin/admin-page";
+    }
+
+    @PostMapping("/admin-page")
     public String createUser(User user) {
         userService.saveUser(user);
-        return "redirect:/adminPage";
+        return "redirect:/admin-page";
     }
 
     @GetMapping("/user-delete/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
         userService.deleteById(id);
-        return "redirect:/adminPage";
+        return "redirect:/admin-page";
     }
 
     @GetMapping("/user-update/{id}")
@@ -47,8 +51,6 @@ public class AdminController {
     @PostMapping("/user-update")
     public String updateUser(User user) {
         userService.updateUser(user);
-        return "redirect:/adminPage";
+        return "redirect:/admin-page";
     }
-
-
 }
