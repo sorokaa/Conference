0979c060bf028b0fcef585b13d48b1e3982551da
sorokaa <sasha20999@outlook.com>
2021-06-20 16:46:20 +0300

    Optimized endpoints

--- src/main/java/com/task/Conference/controllers/MainController.java
+++ src/main/java/com/task/Conference/controllers/MainController.java
@@ -1,22 +1,28 @@
 package com.task.Conference.controllers;
 
-import com.task.Conference.services.UserService;
+import com.task.Conference.entities.Room;
+import com.task.Conference.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class MainController {
 
-    @Autowired
-    private UserService userService;
+    private RoomService roomService;
 
-    @RequestMapping("/")
-    public String mainPage() {
+    @Autowired
+    public MainController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping("/")
+    public String mainPage(Map<String, Object> model) {
+        List<Room> rooms = roomService.findAll();
+        model.put("rooms", rooms);
         return "index";
     }
 
@@ -24,11 +30,4 @@ public class MainController {
     public String registrationPage() {
         return "registration";
     }
-
-    @ResponseBody
-    @RequestMapping("/talksList")
-    public String talksList(Map<String, Object> model) {
-        return "talksList";
-    }
-
 }
